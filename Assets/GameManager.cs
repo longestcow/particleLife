7150f367e1167c7685b75be8d9db405c73f20a49
mambo <omiyer76@gmail.com>
2025-02-07 01:38:05 -0600

    functionality complete

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 
 public class GameManager : MonoBehaviour {
@@ -19,6 +20,7 @@ public class GameManager : MonoBehaviour {
     List<Matrix4x4>[] meshPositions;
     Mesh particleMesh;
     public Material[] materials;
+    public RenderParams[] renderParams;
 
     List<Particle>[,] grid;
 
@@ -28,12 +30,16 @@ public class GameManager : MonoBehaviour {
     int gridx,gridy;
     bool pPressed = false;
     Vector2 pMousePosition = Vector2.zero, difference;
+    ConcurrentBag<Matrix4x4>[] meshPositionsConcurrent;
 
     DynamicLayout matrixUI;
     float[,] matrix;
-    void Start() {
-        dimension = (int)transform.localScale.x;
 
+    void Start() {
+        meshPositionsConcurrent = new ConcurrentBag<Matrix4x4>[typeCount];
+        dimension = (int)transform.localScale.x;
+        transform.position = new Vector2(dimension/2, dimension/2);
+        renderParams = new RenderParams[typeCount];
         particleMesh =  Resources.GetBuiltinResource<Mesh>("Quad.fbx");
         Vector2 size = GetComponent<Renderer>().bounds.size; 
         meshPositions = new List<Matrix4x4>[typeCount];
@@ -49,6 +55,9 @@ public class GameManager : MonoBehaviour {
 
         for (int i = 0; i < typeCount; i++) {
             meshPositions[i] = new List<Matrix4x4>(); 
+            meshPositionsConcurrent[i] = new ConcurrentBag<Matrix4x4>();
+            renderParams[i] = new RenderParams(materials[i]);
+
         }
         
 
@@ -108,37 +117,68 @@ public class GameManager : MonoBehaviour {
             }
         }
 
-        for(int i = 0; i<typeCount; i++)
+        for(int i = 0; i<typeCount; i++){
             meshPositions[i].Clear();
-        
-        for(int r = 0; r<gridDim; r++) {
-            for(int c = 0; c<gridDim; c++){
-                foreach(Particle p1 in grid[r,c]){           //each particle in current grid box
-                    p1.velocity *= friction;
-                    for(int i = -1; i<=1; i++){             
-                        for(int j = -1; j<=1; j++){         //9 grid boxes around current grid box
-                            gridx = (int)nfmod((r+i),gridDim);
-                            gridy = (int)nfmod((c+j),gridDim);
-                            foreach(Particle p2 in grid[gridx,gridy]){
-                                if(p1.id==p2.id)continue;
-                                p1.velocity+= forceMultiplier(p1,p2) * 0.02f;
-                            }
-                        }
-                    }
-                    p1.position += p1.velocity * 0.02f;
-
-                    if(p1.position.x < 0) p1.position.x = dimension-0.1f;
-                    else if (p1.position.x >= dimension) p1.position.x = 0;
-                    if(p1.position.y < 0) p1.position.y = dimension-0.1f;
-                    else if(p1.position.y >= dimension) p1.position.y = 0;
-
-                    meshPositions[p1.type].Add(
-                        Matrix4x4.TRS(p1.position, Quaternion.identity, Vector3.one * particleSize)
-                    );
-                }
-
-            }
+            meshPositionsConcurrent[i].Clear();
         }
+        
+        Parallel.For(0, particles.Length, i=>{
+            particles[i].velocity *=friction;
+        
+            for(int offi = -1; offi<=1; offi++){
+                for(int offj = -1; offj<=1; offj++){
+                    gridx = (int)nfmod((int)(particles[i].position.x/radius)+offi,gridDim);
+                    gridy = (int)nfmod((int)(particles[i].position.y/radius)+offj,gridDim);
+                    foreach(Particle p2 in grid[gridy,gridx]){
+                        if(particles[i].id==p2.id)continue;
+                        particles[i].velocity+= forceMultiplier(particles[i],p2) * 0.02f;
+                    }
+                }
+                    
+            }
+            
+            particles[i].position += particles[i].velocity * 0.02f;
+            if(particles[i].position.x < 0) particles[i].position.x = dimension-0.1f;
+            else if (particles[i].position.x >= dimension) particles[i].position.x = 0;
+            if(particles[i].position.y < 0) particles[i].position.y = dimension-0.1f;
+            else if(particles[i].position.y >= dimension) particles[i].position.y = 0;
+
+            meshPositionsConcurrent[particles[i].type].Add(
+                Matrix4x4.TRS(particles[i].position, Quaternion.identity, Vector3.one * particleSize)
+            );
+
+        });
+
+        // Parallel.For(0, gridDim, r=> {
+        //     for(int c = 0; c<gridDim; c++){
+        //         foreach(Particle p1 in grid[r,c]){           //each particle in current grid box
+        //             p1.velocity *= friction;
+        //             for(int i = -1; i<=1; i++){             
+        //                 for(int j = -1; j<=1; j++){         //9 grid boxes around current grid box
+        //                     gridx = (int)nfmod((r+i),gridDim);
+        //                     gridy = (int)nfmod((c+j),gridDim);
+        //                     foreach(Particle p2 in grid[gridx,gridy]){
+        //                         if(p1.id==p2.id)continue;
+        //                         p1.velocity+= forceMultiplier(p1,p2) * 0.02f;
+        //                     }
+        //                 }
+        //             }
+
+        //             p1.position += p1.velocity * 0.02f;
+
+
+        //             if(p1.position.x < 0) p1.position.x = dimension-0.1f;
+        //             else if (p1.position.x >= dimension) p1.position.x = 0;
+        //             if(p1.position.y < 0) p1.position.y = dimension-0.1f;
+        //             else if(p1.position.y >= dimension) p1.position.y = 0;
+                   
+        //             meshPositionsConcurrent[p1.type].Add(
+        //                 Matrix4x4.TRS(p1.position, Quaternion.identity, Vector3.one * particleSize)
+        //             );
+        //         }
+
+        //     }
+        // });
         for(int i = 0 ;i < gridDim; i++){
             for(int j = 0; j < gridDim; j++)
                 grid[i,j].Clear();
@@ -146,8 +186,11 @@ public class GameManager : MonoBehaviour {
         }
 
         for(int i = 0; i<particleCount; i++){
-            grid[Mathf.Clamp((int)(particles[i].position.y/radius), 0, dimension-1), Mathf.Clamp((int)(particles[i].position.x/radius), 0, dimension-1)].Add(particles[i]);
+            grid[Mathf.Clamp((int)(particles[i].position.y/radius), 0, gridDim-1), Mathf.Clamp((int)(particles[i].position.x/radius), 0, gridDim-1)].Add(particles[i]);
         }
+
+        for (int i = 0; i < typeCount; i++)
+            meshPositions[i] = new List<Matrix4x4>(meshPositionsConcurrent[i]);
 
         render();
     }
@@ -159,9 +202,8 @@ public class GameManager : MonoBehaviour {
                 int count = Mathf.Min(1023, batchSize - j);
                 for(int k = 0; k<count; k++)
                     matrixBuffer[k] = meshPositions[i][j + k];
-
-                Graphics.DrawMeshInstanced(particleMesh, 0, materials[i], matrixBuffer, count);
-            }
+                Graphics.RenderMeshInstanced(renderParams[i], particleMesh, 0, matrixBuffer, count);
+            } 
         }
     }
 
@@ -177,19 +219,19 @@ public class GameManager : MonoBehaviour {
         Vector2 direction = (p2.position - p1.position).normalized;
         float distance = Vector2.Distance(p1.position, p2.position);
         if(distance>radius) {
-            if (Mathf.Abs(delta.x) > transform.localScale.x / 2)
-                delta.x -= Mathf.Sign(delta.x) * transform.localScale.x;
+            if (Mathf.Abs(delta.x) > dimension / 2)
+                delta.x -= Mathf.Sign(delta.x) * dimension;
 
-            if (Mathf.Abs(delta.y) > transform.localScale.y / 2)
-                delta.y -= Mathf.Sign(delta.y) * transform.localScale.y;
+            if (Mathf.Abs(delta.y) > dimension / 2)
+                delta.y -= Mathf.Sign(delta.y) * dimension;
 
             distance = delta.magnitude;
             direction = delta.normalized;
             if(distance>radius)return Vector2.zero;
         }
         if(distance<tooCloseR) return mapfloat(distance,  0, tooCloseR, -1, 0) * direction * forceScale; 
-        else if(distance<=radius/2) force = mapfloat(distance, tooCloseR, radius/2, 0, 1);
-        else force = mapfloat(distance,  radius/2, radius, 1, 0);
+        else if(distance<=tooCloseR+((radius-tooCloseR)/2)) force = mapfloat(distance, tooCloseR, tooCloseR+((radius-tooCloseR)/2), 0, 1);
+        else force = mapfloat(distance,  tooCloseR+((radius-tooCloseR)/2), radius, 1, 0);
         force*=matrix[p2.type, p1.type]; 
         return force * direction * forceScale;
         
